e7e7b3cc857d79319368128dddb84583a84bf1ff
Rasul Mahammadaliyev <106903664+rasulmah@users.noreply.github.com>
2025-02-25 10:16:50 +0400

    Responsive Card
    I made it responvie featured Col Card

--- src/components/Cards/FeaturedColCard.jsx
+++ src/components/Cards/FeaturedColCard.jsx
@@ -4,7 +4,19 @@ import '../../assets/scss/_featuredColCard.scss'
 
 const FeaturedColCard = () => {
   return (
-    <div className='featured-card-container'>FeaturedCard</div>
+    <div className='featured-card-container d-flex flex-column align-items-left justify-content-end'>
+      <div className="t-body d-flex flex-column align-items-left justify-content-center ">
+        <h3 className="category category-heading">Caps & Hats</h3>
+        <p className="category-subtext">
+        The iconic Mercedes-AMG F1 Team Cap is the best way to complete an outfit
+        </p>
+        <a className='shp-text d-flex'>
+          <p>Shop</p> 
+          <p className='category'>Caps & Hats</p>
+        </a>
+      </div>
+      
+    </div>
   )
 }
 
